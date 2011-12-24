0b30efdab7a03a201e19b4684882b458e42e7213
hvelarde <hector.velarde@gmail.com>
2011-12-23 19:02:45 -0600

    faltaba agregar al paquete inicial

---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='telesur.contenttypes',
+      version=version,
+      description="Tipos de contenido para teleSUR",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Plone :: 4.1",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        ],
+      keywords='plone dexterity',
+      author='Héctor Velarde',
+      author_email='hector.velarde@gmail.com',
+      url='https://github.com/desarrollotv/telesur.contenttypes',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['telesur'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+        'setuptools',
+        'plone.app.dexterity>=1.1',
+        'plone.app.referenceablebehavior',
+        ],
+      extras_require={
+        'test': ['plone.app.testing'],
+        },
+      entry_points="""
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
