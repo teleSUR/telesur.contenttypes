928a88f25ed4ce082d867f1118145e19b0d3242c
hvelarde <hector.velarde@gmail.com>
2011-12-24 11:33:59 -0600

    se agregó prueba para el comportamiento referenceable

--- src/telesur/contenttypes/tests/test_program.py
+++ src/telesur/contenttypes/tests/test_program.py
@@ -7,7 +7,10 @@ from zope.component import queryUtility
 
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
+
+from plone.app.referenceablebehavior.referenceable import IReferenceable
 from plone.dexterity.interfaces import IDexterityFTI
+from plone.uuid.interfaces import IAttributeUUID
 
 from telesur.contenttypes.program import IProgram
 from telesur.contenttypes.testing import INTEGRATION_TESTING
@@ -43,6 +46,12 @@ class IntegrationTest(unittest.TestCase):
         factory = fti.factory
         new_object = createObject(factory)
         self.failUnless(IProgram.providedBy(new_object))
+
+    def test_is_referenceable(self):
+        self.folder.invokeFactory('telesur.contenttypes.program', 'p1')
+        p1 = self.folder['p1']
+        self.assertTrue(IReferenceable.providedBy(p1))
+        self.assertTrue(IAttributeUUID.providedBy(p1))
 
 
 def test_suite():
