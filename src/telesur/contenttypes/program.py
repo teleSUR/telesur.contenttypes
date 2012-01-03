74614bac88c2bcd6eba683688d5a9801e394993f
hvelarde <hector.velarde@gmail.com>
2012-01-03 13:31:44 -0600

    se agregaron un par de campos más al tipo de contenido programa

--- src/telesur/contenttypes/program.py
+++ src/telesur/contenttypes/program.py
@@ -23,20 +23,37 @@ class IProgram(form.Schema):
     url = schema.TextLine(
             title=_(u'URL'),
             description=_(u'help_url',
-                          default=u"Program's page URL in video system."),
+                          default=u"URL of the frontpage in the video "
+                                   "system."),
+            required=False,
+        )
+
+    host = schema.TextLine(
+            title=_(u'Host'),
+            description=_(u'help_host',
+                          default=u"The name of the host."),
             required=False,
         )
 
     email = schema.TextLine(
             title=_(u'Email'),
             description=_(u'help_email',
-                          default=u"Program's host e-mail address."),
+                          default=u"Email address of the host."),
             required=False,
         )
 
     twitter = schema.TextLine(
             title=_(u'Twitter account'),
             description=_(u'help_twitter',
-                          default=u"Program's Twitter account."),
+                          default=u"Twitter account associated with the "
+                                   "program."),
+            required=False,
+        )
+
+    widget = schema.TextLine(
+            title=_(u'Widget'),
+            description=_(u'help_widget',
+                          default=u"URL of the widget that shows the most "
+                                   "recent programs."),
             required=False,
         )
